2d2083eb6dde9da4ec5764132bc661598dc4c3ab
username <inbox@email.tld>
2017-10-17 22:10:09 +0700

    Comment

--- script.ps1
+++ script.ps1
@@ -7,7 +7,7 @@ Function Get-RandomHostname()
     #Symbols in ASCII character set, always started from letters, 15 symbols length, uses all upper-case letters and 0-9 numbers
     #Exclude symbols: "073 I", "079 O"
     $RandomHostname = (-join (((65..72) + (74..78) + (80..90)) | Get-Random -Count 1 | % {[char]$_})) + (-join ((65..72) + (74..78) + (80..90) + (48..57) | Get-Random -Count 14 | % {[char]$_}))
-    Write-Host $RandomHostname
+    Return $RandomHostname
 } #end Get-RandomHostname
 
-1..10 | % {Get-RandomHostname}
+1..10 | % {Write-Host $(Get-RandomHostname)}
