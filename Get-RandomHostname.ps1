165565d23e2cd187f249d1060e07225d7e143f4b
username <inbox@email.tld>
2017-12-28 02:16:32 +0700

    Comment

--- Get-RandomHostname.ps1
+++ Get-RandomHostname.ps1
@@ -3,11 +3,11 @@
 
 # --------------------------------------------------------------
 # SCRIPT           : Get-RandomHostname.ps1
-# DESCRIPTION      : Generate random hostname
+# DESCRIPTION      : Generate random hostname function
 # DATE             : 22-Oct-2017
 # AUTHOR           : Alseg
-# VERSION          : 0.4a
-# KEYWORDS         : Hostname, Random, Generate
+# VERSION          : 0.5a
+# KEYWORDS         : Hostname, Random, Generate, Function
 # --------------------------------------------------------------
 
 #Function Get-RandomHostname
@@ -15,24 +15,24 @@ Function Get-RandomHostname
 {
     <#
     .DESCRIPTION
-        Generate random hostname
+        Generate random hostname function.
     .PARAMETER Length
-        (Optionally) Length of hostname string (default is 15)
+        (Optionally) Length of hostname string (default is 15).
     .PARAMETER Number
-        (Optionally) Number output random hostname strings (default is 15)
+        (Optionally) Number output random hostname strings (default is 1).
     .NOTES
-        Generate new hostname string
-        Symbols in ASCII character set, always started from letters, 15 symbols length, uses all upper-case letters and 0-9 numbers
-        Exclude symbols: "073 I", "079 O"
+        Generate new hostname string.
+        Symbols in ASCII character set, always started from letters, upper-case letters and 0-9 numbers.
+        (Exclude symbols: "073 I", "079 O").
     .EXAMPLE
         Import-Module <path>
-        Get-RandomHostname
+        Write-Host (Get-RandomHostname)
     .EXAMPLE
         Import-Module <path>
-        Get-RandomHostname -l 22
+        Write-Host (Get-RandomHostname -l 22)
     .EXAMPLE
         Import-Module <path>
-        Get-RandomHostname -l 5 -n 20
+        Write-Host (Get-RandomHostname -l 5 -n 20)
     #>
 
     #Argument for command line request
@@ -44,12 +44,19 @@ Function Get-RandomHostname
         [ValidateRange(1,200)]
         [Alias("n")]
         [Int]$Number = 1
-        )#End Param
+        ) #End Param
+
+    #Function body
+    $Result = @()
 
     for ($i = 1; $i -le $Number; $i++)
         {
-            $RandomHostname = (-join (((65..72) + (74..78) + (80..90)) | Get-Random -count 1 | ForEach-Object {[char]$_})) + `
-            (-join ((65..72) + (74..78) + (80..90) + (48..57) | Get-Random -count ($Length - 1) | ForEach-Object {[char]$_}))
-            Write-Host $RandomHostname
+            $RandomHostname = (-join (((65..72) + (74..78) + (80..90)) | Get-Random -Count 1 | ForEach-Object {[char]$_})) + `
+            (-join ((65..72) + (74..78) + (80..90) + (48..57) | Get-Random -Count ($Length - 1) | ForEach-Object {[char]$_}))
+            $Result += $RandomHostname
         }
-}#End Get-RandomHostname
+    #End of function body
+
+    #Return
+    Return $Result
+} #End Get-RandomHostname
